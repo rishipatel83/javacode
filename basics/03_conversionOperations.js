9b73f26944ae339c91e7616352fab4890637561e
Rishi <rishi.777.patel@gmail.com>
2025-12-18 19:42:28 +0530

    conversion of datatypes

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn = "rishi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// "" => false
+// "rishi" => true
