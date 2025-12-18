dbcd73ef8934059e7b0c3c6b1d72e60303d3b226
Rishi <rishi.777.patel@gmail.com>
2025-12-18 19:27:56 +0530

    first config

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "rishi"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+
+//object 
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 12445
+let accountEmail = "rishi@gmail.com"
+var accountPassword 
+accountCity = "Jabalpur"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// Prefer not to use because scope in block and functional scope
