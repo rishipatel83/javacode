34caaa86990bfb72f12ae6656b9c1165bfd91f72
Rishi <rishi.777.patel@gmail.com>
2025-12-19 11:00:48 +0530

    datatypes name updated

--- basics/variables.js
+++
@@ -1,8 +0,0 @@
-const accountId = 12445
-let accountEmail = "rishi@gmail.com"
-var accountPassword 
-accountCity = "Jabalpur"
-console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity])
-
-// Prefer not to use because scope in block and functional scope
