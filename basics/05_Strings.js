80eb2c7b3ba3e54bdd2e8d4fcd84ab477e76c339
Rishi <rishi.777.patel@gmail.com>
2025-12-19 11:01:16 +0530

    strings basics

---
+++ basics/05_Strings.js
@@ -0,0 +1,13 @@
+const getName = new String('rishicodes')
+// console.log(getName.toUpperCase())
+// console.log(getName.length)
+// console.log(getName.charAt(2))
+// console.log(getName.bold);
+const url = "https://rishipatel/rishi%20patel"
+// console.log(url.replace('%20','-'));
+// console.log(url.split('/'));
+const trim = "   rishi    ";
+// console.log(trim.trim())
+const name = "rishi"
+const repoCount = 50;
+console.log(`My name is ${name} and my repocount in my github is ${repoCount} repo's`)
