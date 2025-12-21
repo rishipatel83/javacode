5e4f996ea007ed275d8d1990240461a1f3ac2223
Rishi <rishi.777.patel@gmail.com>
2025-12-21 09:26:00 +0530

    added join method

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,11 +6,13 @@ console.log(myHeros[0])
 
 //  +++++++++++ Methods in Array +++++++++++++++++
 
-//* methods like push,pop,unshift,shift
+//* methods like push,pop,unshift,shift ,includes,indexOf
 // slice doesn't change the original array and gives us the answer of the array
 // splice cut down the original array as given below
 
 let arr = [0,1,2,3,4,5]
+const myArr = arr.join() //* returns us the value of all arrays in string with each dicrete value
+console.log(myArr)
 console.log(arr)
 let myn1 = arr.slice(1,3);    //
 console.log("Original after using slice",arr)
