782b6bd0a8b2ced811013e026675260b75e46da1
Rishi <rishi.777.patel@gmail.com>
2025-12-22 19:23:57 +0530

    learnt about switch statements

---
+++ control_flow/switch.js
@@ -0,0 +1,14 @@
+let guess = ""
+switch(guess){
+   case "rishi":
+      console.log("your guess is rishi")
+      break;
+   case "harry":
+      console.log("your guess is harry")
+      break
+   case "hitesh":
+      console.log("your guess is hitesh")
+      break
+   default :
+      console.log("your doesn't match any cases")
+}
